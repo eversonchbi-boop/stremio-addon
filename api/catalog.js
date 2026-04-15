58351b0145779977e0a58a4055d82d11c4f68055
eversonchbi-boop <eversonchbi@gmail.com>
2026-04-15 12:28:20 -0300

    Update catalog.js

--- api/catalog.js
+++ api/catalog.js
@@ -1,29 +1,30 @@
-import { getData } from "./_csv.js";
-
 export default async function handler(req, res) {
-  const { searchParams } = new URL(req.url);
-  const type = searchParams.get("type");
-  const id = searchParams.get("id");
+  const { searchParams } = new URL(req.url)
+  const type = searchParams.get("type")
+  const id = searchParams.get("id")
 
-  const rows = await getData();
+  const resCsv = await fetch("https://docs.google.com/spreadsheets/d/1w4Aoszpli2vz7FowLuMYgYwgEpn5GUkhUk9WrQK2E0c/export?format=csv&gid=0")
+  const text = await resCsv.text()
+
+  const rows = text.split("\n").slice(1).map(r => r.split(","))
 
   const items = rows.map(r => ({
     id: r[0],
     type: r[1],
     title: r[2],
     category: r[6]
-  }));
+  }))
 
-  const filtered = items.filter(i => i.type === type && i.category === id);
+  const filtered = items.filter(i => i.type === type && i.category === id)
 
-  const unique = {};
-  filtered.forEach(i => unique[i.id] = i);
+  const unique = {}
+  filtered.forEach(i => unique[i.id] = i)
 
   const metas = Object.values(unique).map(i => ({
     id: i.id,
     type: i.type,
     name: i.title
-  }));
+  }))
 
-  res.json({ metas });
+  res.json({ metas })
 }

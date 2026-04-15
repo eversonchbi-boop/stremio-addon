4f19d7b64f3a1b73623a85bd01d399e196275213
eversonchbi-boop <eversonchbi@gmail.com>
2026-04-15 12:24:48 -0300

    Update _csv.js

--- api/_csv.js
+++ api/_csv.js
@@ -1,30 +1,8 @@
-let cache = null;
-let lastFetch = 0;
-
-const SHEET = "https://docs.google.com/spreadsheets/d/1w4Aoszpli2vz7FowLuMYgYwgEpn5GUkhUk9WrQK2E0c/export?format=csv&gid=0";
-
-function parseCSV(text) {
-  const lines = text.trim().split("\n");
-
-  return lines.slice(1).map(line => {
-    return line.split(",");
-  });
-}
-
 export async function getData() {
-  const now = Date.now();
-
-  if (cache && now - lastFetch < 300000) {
-    return cache;
-  }
-
-  const res = await fetch(SHEET);
+  const res = await fetch("https://docs.google.com/spreadsheets/d/1w4Aoszpli2vz7FowLuMYgYwgEpn5GUkhUk9WrQK2E0c/export?format=csv&gid=0");
   const text = await res.text();
 
-  const rows = parseCSV(text);
-
-  cache = rows;
-  lastFetch = now;
+  const rows = text.split("\n").slice(1).map(r => r.split(","));
 
   return rows;
 }

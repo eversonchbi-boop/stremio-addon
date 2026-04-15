23ef080d68d1c9355b439788f754c40fa5936b49
eversonchbi-boop <eversonchbi@gmail.com>
2026-04-15 15:49:38 -0300

    Update manifest.js

--- api/manifest.js
+++ api/manifest.js
@@ -1,3 +1,16 @@
 export default function handler(req, res) {
-  res.json({ ok: true })
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.json({
+          id: 'community.evfilmes',
+          version: '1.0.0',
+          name: 'EV Filmes',
+          description: 'Filmes e series via torrent da planilha EV',
+          resources: ['catalog', 'meta', 'stream'],
+          types: ['movie', 'series'],
+          catalogs: [
+            { type: 'movie', id: 'ev-movies', name: 'EV Filmes' },
+            { type: 'series', id: 'ev-series', name: 'EV Series' }
+                ],
+          idPrefixes: ['ev-']
+    })
 }
